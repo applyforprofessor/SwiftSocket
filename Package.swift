3c506cdbcff61b20ba9dac529cf169c0805a2553
Pip-Boy <applyforprofessor@icloud.com>
2020-08-14 16:43:49 +0800

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftSocket",
+    products: [
+        .library(name: "SwiftSocket", targets: ["SwiftSocket"])
+    ],
+    targets: [
+        .target(name: "SwiftSocket"),
+        .testTarget(name: "SwiftSocket", dependencies: ["SwiftSocket"])
+    ]
+)
